19ce65dd93d60af757f32192d4127436beb52092
Vadim Comanescu <vadim984@gmail.com>
2011-10-12 01:20:03 +0300

    First test

---
+++ test/ro/vadim/goos/test/First.java
@@ -0,0 +1,14 @@
+package ro.vadim.goos.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class First {
+	
+	@Test
+	public void shouldNotDoShit() {
+		assertEquals("1", "1");
+	}
+}
