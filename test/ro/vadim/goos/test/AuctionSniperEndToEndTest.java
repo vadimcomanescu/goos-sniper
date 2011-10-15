92d55b503f0e9348d51ff05c6b22dcaa65bb1c89
Vadim Comanescu <vadim984@gmail.com>
2011-10-15 16:46:37 +0300

    Added external libraries and first class for passing the walking skeleton.

---
+++ test/ro/vadim/goos/test/AuctionSniperEndToEndTest.java
@@ -0,0 +1,28 @@
+package ro.vadim.goos.test;
+
+import org.junit.After;
+import org.junit.Test;
+
+
+public class AuctionSniperEndToEndTest {
+	private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+	private final ApplicationRunner application = new ApplicationRunner();
+	
+	@Test public void sniperJoinAuctionUntilAuctionClose() throws Exception{
+		auction.startSellingItem();
+		application.startBiddingIn(auction);
+		auction.hasReceivedJoinRequestFromSniper();
+		auction.announceClose();
+		application.showsSniperHasLostAuction();
+	}
+	
+	@After public void stopAuction() { 
+		auction.stop();
+	}
+	
+	@After public void stopApplication() {
+		application.stop();
+	}
+	
+
+}
---
+++ test/ro/vadim/goos/test/FakeAuctionServer.java
@@ -0,0 +1,9 @@
+package ro.vadim.goos.test;
+
+public class FakeAuctionServer {
+
+	public FakeAuctionServer(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
